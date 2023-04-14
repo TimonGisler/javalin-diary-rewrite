a7b85e35f65945e99d26b193faa786e253bb7445
Your Name <timongisler@icloud.com>
2023-04-15 00:17:25 +0200

    fetching now works too, probably good template, logging, migration, quering and saving works

--- src/main/kotlin/entries/GetAllEntriesQuery.kt
+++ src/main/kotlin/entries/GetAllEntriesQuery.kt
@@ -1,23 +1,24 @@
 package entries
 
 import io.javalin.http.Context
+import jdbi
 
 object GetAllEntriesQuery {
 
     // return value
     data class Entry(val text: String, val title: String)
-    data class AllEntries(val entries: List<Entry>)
-
     fun getAllEntriesHandler(ctx: Context){
-        val entries = fetchAllEntries(1)
+        println("Moin")
+        val entries = fetchAllEntries()
         ctx.json(entries)
     }
 
-    private fun fetchAllEntries(userId: Long): AllEntries{
-        return AllEntries(listOf(
-            Entry("text", "title"),
-            Entry("text2", "title2"),
-            Entry("text3", "title3"),
-        ))
+    private fun fetchAllEntries(): List<Entry> {
+        return jdbi.withHandle<List<Entry>, Exception> {
+            it.createQuery("SELECT * FROM entry")
+                .mapTo(Entry::class.java)
+                .list()
+
+        }
     }
 }
