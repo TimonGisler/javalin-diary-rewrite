37994796fa15fe0029ac6bfb7be374be7b32e807
Your Name <timongisler@icloud.com>
2023-04-23 22:46:47 +0200

    moved common code into abstract class

---
+++ src/test/kotlin/helperClasses/PostgresContainerBaseTest.kt
@@ -0,0 +1,39 @@
+package helperClasses
+
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.BeforeEach
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+/**
+ * sets up a postgres db in a docker container and migrates it to the latest version.
+ * Additionally, it sets up a jdbi instance that uses the test db.
+ * This is done before every single @Test method (before every method call the db and the jdbi get reset).
+ */
+@Testcontainers
+abstract class PostgresContainerBaseTest {
+
+    // will be started before and stopped after each test method (must be static that that does not happen)
+    @Container
+    var postgresContainer: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:15.2-alpine")
+        .withExposedPorts(5432)
+        .withDatabaseName("testDbName")
+        .withUsername("testUserName")
+        .withPassword("testPw")
+
+    @BeforeEach
+    fun setupDb() {
+        val jdbcUrl = postgresContainer.jdbcUrl
+        val username = postgresContainer.username
+        val password = postgresContainer.password
+
+        Flyway.configure()
+            .dataSource(jdbcUrl, username, password)
+            .loggers("slf4j")
+            .load()
+            .migrate()
+
+        JdbiProvider.setupTestingJdbi(jdbcUrl, username, password)
+    }
+}
